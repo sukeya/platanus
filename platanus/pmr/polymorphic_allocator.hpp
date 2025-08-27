3edbf1223d6220080ae7b9b487c537708e035f1c
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2025-08-27 23:04:47 +0900

    Fix the period of copyright.

--- platanus/pmr/polymorphic_allocator.hpp
+++ platanus/pmr/polymorphic_allocator.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024- Yuya Asano <my_favorite_theory@yahoo.co.jp>
+// Copyright 2024-2025 Yuya Asano <my_favorite_theory@yahoo.co.jp>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

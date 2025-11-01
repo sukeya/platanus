05c0a06fbb57670d17accc7e8ff91d364d3653ef
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2025-11-01 22:53:59 +0900

    Fix pointer and reference member type of const iterator.

--- include/platanus/internal/btree_iterator.hpp
+++ include/platanus/internal/btree_iterator.hpp
@@ -157,9 +157,7 @@ struct btree_iterator<Node, false> {
   using key_type        = typename Node::key_type;
   using value_type      = typename Node::value_type;
   using reference       = typename Node::reference;
-  using const_reference = typename Node::const_reference;
   using pointer         = typename Node::pointer;
-  using const_pointer   = typename Node::const_pointer;
 
   using size_type         = typename Node::size_type;
   using difference_type   = typename Node::difference_type;
@@ -228,10 +226,8 @@ struct btree_iterator<Node, true> {
 
   using key_type        = typename Node::key_type;
   using value_type      = typename Node::value_type;
-  using reference       = typename Node::reference;
-  using const_reference = typename Node::const_reference;
-  using pointer         = typename Node::pointer;
-  using const_pointer   = typename Node::const_pointer;
+  using reference       = typename Node::const_reference;
+  using pointer         = typename Node::const_pointer;
 
   using size_type         = typename Node::size_type;
   using difference_type   = typename Node::difference_type;
@@ -260,11 +256,11 @@ struct btree_iterator<Node, true> {
     return key(node, position);
   }
 
-  const_reference operator*() const {
+  reference operator*() const {
     using internal::value;
     return value(node, position);
   }
-  const_pointer operator->() const { return &(this->operator*()); }
+  pointer operator->() const { return &(this->operator*()); }
 
   self_type& operator++() noexcept {
     btree_iterator_impl::increment(node, position);
